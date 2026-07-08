cd4bbe34ec95736b3d323a2ae85b473fd42016aa
seungjin <xeungjin@nate.com>
2026-07-08 12:20:14 +0900

    DB connection

--- src/main/java/com/example/demo/config/SecurityConfiguration.java
+++ src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -5,24 +5,56 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration {
 
-    @Bean
-    public UserDetailsService userDetailsServiceInMemory() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password("{noop}password")
-                .roles("USER")
-                .build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password("{noop}password")
-                .roles("USER", "ADMIN")
-                .build();
+//    @Bean
+//    public UserDetailsService userDetailsServiceInMemory() {
+//        UserDetails user = User.builder()
+//                .username("user")
+//                .password("{noop}password")
+//                .roles("USER")
+//                .build();
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password("{noop}password")
+//                .roles("USER", "ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+//        UserDetails user = User.builder()
+//                .username("user")
+//                .password(passwordEncoder.encode("password"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder.encode("password"))
+//                .roles("USER", "ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
 
-        return new InMemoryUserDetailsManager(user, admin);
+    @Bean
+    UserDetailsManager userDetailsManagerJdbc(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
