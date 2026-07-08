8b07881647a10581fe770e066b1cf74d899d0895
seungjin <xeungjin@nate.com>
2026-07-08 11:06:35 +0900

    381page

---
+++ src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public UserDetailsService userDetailsServiceInMemory() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}password")
+                .roles("USER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
