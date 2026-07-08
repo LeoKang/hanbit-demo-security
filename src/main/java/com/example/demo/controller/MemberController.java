ed769b72fc008e39ce8c08d722e25ae61c7f09c3
seungjin <xeungjin@nate.com>
2026-07-08 10:20:36 +0900

    372page

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -12,7 +12,10 @@ import java.util.List;
 @Slf4j
 public class MemberController {
     private List<Member> members = List.of(
-            new Member(1L, "윤서준", "SeojunYoon@hanbit.co.kr", null)
+            new Member(1L, "윤서준", "SeojunYoon@hanbit.co.kr", null),
+            new Member(2L, "윤광철", "KwangcheolYoon@hanbit.co.kr", null),
+            new Member(3L, "공미영", "MiyeongKong@hanbit.co.kr", null),
+            new Member(4L, "김도윤", "DoyunKim@hanbit.co.kr", null)
     );
 
     @GetMapping("/member/list")
